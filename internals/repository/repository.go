e8454f3934b354de2455447f7bfd8a702c5771a8
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-11-25 12:57:36 +0545

    feat: add db method for create,get, update and delete categories

--- internals/repository/repository.go
+++ internals/repository/repository.go
@@ -1,3 +1,13 @@
 package repository
 
-type Repository interface{}
+import "github.com/ishanshre/gomerce/internals/model"
+
+type Repository interface {
+
+	// category interface
+	CreateCategory(name string) (*model.Category, error)
+	GetCategories() ([]*model.Category, error)
+	GetCategory(id int) (*model.Category, error)
+	DeleteCategory(id int) error
+	UpdateCategory(update_data *model.Category) (*model.Category, error)
+}
